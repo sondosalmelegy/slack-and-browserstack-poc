2815fb0ad346138cac98c87412c50db0572506ff
SondosNasserJoda <sondos.nasser@breadfast.com>
2025-08-25 19:45:48 +0300

    Adding the first project

---
+++ src/test/java/BaseTests.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseTests {
+    static ChromeDriver driver = new ChromeDriver();
+    public static void waitingTime() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(120));
+    }
+}
---
+++ src/test/java/SimpleSeleniumTest.java
@@ -0,0 +1,45 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class SimpleSeleniumTest extends BaseTests {
+
+    static ChromeDriver driver = new ChromeDriver();
+
+    @BeforeClass
+    public void setUp(){
+        waitingTime();
+        driver.get("https://www.booking.com/");
+        waitingTime();
+        driver.manage().window().fullscreen();
+        waitingTime();
+    }
+
+    @Test
+    public void testCases() {
+        waitingTime();
+       String text = driver.findElement(By.cssSelector("span[class='b98ba2834c f77a73f1ba f0a26771c4']")).getText();
+
+        Assert.assertEquals(text,"Find your next stay","Passed Test Case!");
+        System.out.println(text);
+    }
+
+    @AfterClass
+    public void teardown() {
+        driver.quit();
+    }
+}
+
+
+
+
+
