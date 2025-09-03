109de0740d1b36bdd55e6efa2e001dba993f4d8e
SondosNasserJoda <sondos.nasser@breadfast.com>
2025-09-03 17:27:30 +0300

    last build

--- src/test/java/SimpleSeleniumTest.java
+++ src/test/java/SimpleSeleniumTest.java
@@ -1,34 +1,25 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 
 public class SimpleSeleniumTest extends BaseTests {
 
     @BeforeClass
     public void setUp(){
         WebDriverManager.chromedriver().setup();
-        System.out.println("Starting test...");
 
         driver = new ChromeDriver();
-        System.out.println("Starting test...");
 
         waitingTime();
         driver.get("https://www.booking.com/");
-        System.out.println("Starting test...");
 
         waitingTime();
         driver.manage().window().fullscreen();
-        System.out.println("Starting test...");
 
         waitingTime();
     }
@@ -36,10 +27,8 @@ public class SimpleSeleniumTest extends BaseTests {
     @Test
     public void testCases() {
         waitingTime();
-        System.out.println("Starting test...");
 
         String text = driver.findElement(By.cssSelector("span[class='b98ba2834c f77a73f1ba f0a26771c4']")).getText();
-        System.out.println("Starting test...");
 
         Assert.assertEquals(text,"Find your next stay","Passed Test Case!");
         System.out.println(text);
