c0eaa044774369f9f0e6140f3be4489a2fc9bd99
SondosNasserJoda <sondos.nasser@breadfast.com>
2025-09-03 19:33:16 +0300

    final

--- src/test/java/SimpleSeleniumTest.java
+++ src/test/java/SimpleSeleniumTest.java
@@ -9,37 +9,18 @@ import org.testng.annotations.Test;
 
 
 public class SimpleSeleniumTest extends BaseTests {
-    ChromeOptions options;
-
 
     @BeforeClass
     public void setUp() {
 
-        waitingTime();
-        options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--ignore-certificate-errors");
-        options.addArguments("--disable-popup-blocking");
-        options.addArguments("--disable-extensions");
-        options.setAcceptInsecureCerts(true);
-        options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
-                "AppleWebKit/537.36 (KHTML, like Gecko) " +
-                "Chrome/118.0.5993.90 Safari/537.36");
-
-
-        waitingTime();
         WebDriverManager.chromedriver().setup();
 
         waitingTime();
-        driver = new ChromeDriver(options);
-        waitingTime();
-
+        driver = new ChromeDriver();
         waitingTime();
         driver.get("https://www.booking.com/");
-
         waitingTime();
         driver.manage().window().fullscreen();
-
         waitingTime();
     }
 
@@ -47,9 +28,8 @@ public class SimpleSeleniumTest extends BaseTests {
     @Test
     public void testCases() {
         waitingTime();
-
         String text = driver.findElement(By.cssSelector("span[class='b98ba2834c f77a73f1ba f0a26771c4']")).getText();
-
+        waitingTime();
         Assert.assertEquals(text,"Find your next stay","Passed Test Case!");
         System.out.println(text);
     }
