37c93770d7ebd5b45eb5e1b8ce1d19487732e16d
SondosNasserJoda <sondos.nasser@breadfast.com>
2025-09-07 11:45:26 +0300

    Notifying Browserstack and slack

--- src/test/java/BaseTests.java
+++ src/test/java/BaseTests.java
@@ -1,45 +1,54 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
+import java.net.URL;
+import java.net.MalformedURLException;
 import java.time.Duration;
 
 public class BaseTests {
 
-        protected static WebDriver driver;
+    protected static WebDriver driver;
 
-        // Setup للـ driver
-        public static void initializeDriver() {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--ignore-certificate-errors");
-            options.addArguments("--headless=new");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--remote-allow-origins=*");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--window-size=1920,1080");
+    public static void initializeDriver() throws MalformedURLException {
+        String username = System.getenv("BROWSERSTACK_USERNAME");
+        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+        String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
+        String projectName = System.getenv("BROWSERSTACK_PROJECT_NAME");
 
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--ignore-certificate-errors");
+        options.addArguments("--headless=new");   // يشتغل headless
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        options.setAcceptInsecureCerts(true);
 
-            options.setAcceptInsecureCerts(true);
+        options.setCapability("browserName", "chrome");
+        options.setCapability("browserVersion", "latest");
+        options.setCapability("build", buildName);
+        options.setCapability("project", projectName);
+        options.setCapability("name", "Sample Test"); // اسم التست في dashboard
 
-            driver = new ChromeDriver(options);
-            driver.manage().window().maximize();
-        }
-
-        // Explicit wait لعنصر لحد ما يبقى visible
-        public WebElement waitForElement(By locator, int timeoutInSeconds) {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
-            return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
-        }
-
-        // Explicit wait لعنصر لحد ما يبقى clickable
-        public WebElement waitForClickable(By locator, int timeoutInSeconds) {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
-            return wait.until(ExpectedConditions.elementToBeClickable(locator));
-        }
+        driver = new RemoteWebDriver(
+                new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"),
+                options
+        );
     }
 
+    public WebElement waitForElement(By locator, int timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public WebElement waitForClickable(By locator, int timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+}
