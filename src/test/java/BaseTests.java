d7b066cfd74820fabcf58cad941ea29e8665361a
SondosNasserJoda <sondos.nasser@breadfast.com>
2025-09-07 13:12:13 +0300

    Notifying Browserstack and slack

--- src/test/java/BaseTests.java
+++ src/test/java/BaseTests.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.net.URL;
 import java.net.MalformedURLException;
 import java.time.Duration;
+import java.util.HashMap;
 
 public class BaseTests {
 
@@ -23,15 +24,29 @@ public class BaseTests {
         String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
         String projectName = System.getenv("BROWSERSTACK_PROJECT_NAME");
 
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--ignore-certificate-errors");
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        options.setAcceptInsecureCerts(true);
+
+// If you still want DesiredCapabilities for BrowserStack:
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("browserName", "chrome");
         caps.setCapability("browserVersion", "latest");
         caps.setCapability("build", buildName);
         caps.setCapability("project", projectName);
-        caps.setCapability("name", "Sample Test"); // اسم التست هيبان في الـ dashboard
+        caps.setCapability("name", "Sample Test");
 
-        ChromeOptions options = new ChromeOptions();
-        options.merge(caps);
+// Merge ChromeOptions into DesiredCapabilities
+        caps.setCapability(ChromeOptions.CAPABILITY, options);
+
+
+
 
         driver = new RemoteWebDriver(
                 new URL("https://" + USERNAME + ":" + ACCESSKEY + "@hub.browserstack.com/wd/hub"),
