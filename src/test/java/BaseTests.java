ba558890e74f02941d2333f6aafd02ac07bcc37f
SondosNasserJoda <sondos.nasser@breadfast.com>
2025-09-07 11:54:41 +0300

    Notifying Browserstack and slack

--- src/test/java/BaseTests.java
+++ src/test/java/BaseTests.java
@@ -14,33 +14,38 @@ public class BaseTests {
 
     protected static WebDriver driver;
 
-    public static void initializeDriver() throws MalformedURLException {
-        String username = System.getenv("BROWSERSTACK_USERNAME");
-        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-        String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
-        String projectName = System.getenv("BROWSERSTACK_PROJECT_NAME");
+    public static void initializeDriver() {
+        try {
+            String username = System.getenv("BROWSERSTACK_USERNAME");
+            String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+            String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
+            String projectName = System.getenv("BROWSERSTACK_PROJECT_NAME");
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--ignore-certificate-errors");
-        options.addArguments("--headless=new");   // يشتغل headless
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--window-size=1920,1080");
-        options.setAcceptInsecureCerts(true);
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--ignore-certificate-errors");
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            options.setAcceptInsecureCerts(true);
 
-        options.setCapability("browserName", "chrome");
-        options.setCapability("browserVersion", "latest");
-        options.setCapability("build", buildName);
-        options.setCapability("project", projectName);
-        options.setCapability("name", "Sample Test"); // اسم التست في dashboard
+            options.setCapability("browserName", "chrome");
+            options.setCapability("browserVersion", "latest");
+            options.setCapability("build", buildName);
+            options.setCapability("project", projectName);
+            options.setCapability("name", "Sample Test");
 
-        driver = new RemoteWebDriver(
-                new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"),
-                options
-        );
+            driver = new RemoteWebDriver(
+                    new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"),
+                    options
+            );
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Invalid BrowserStack hub URL", e);
+        }
     }
+
 
     public WebElement waitForElement(By locator, int timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
